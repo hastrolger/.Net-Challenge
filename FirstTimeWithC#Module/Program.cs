d60c78cf932396a987ea78fba756c7b9c1170b05
hastrolger <parralesjael@gmail.com>
2023-05-26 11:19:55 -0500

    Writing C# code for the first time

---
+++ FirstTimeWithC#Module/Program.cs
@@ -0,0 +1,25 @@
+﻿// DATA TYPES
+// declare variable 
+int amountOfProduct;
+amountOfProduct = 3;
+
+decimal price = 1.2m;
+
+
+// print on console the value of number 
+// Write method set the cursor pointer on the same line
+Console.Write(amountOfProduct);
+
+// formatting output 
+// WriteLine method set the cursor pointer on the next line
+Console.WriteLine("\nHello, World!");
+
+// print hello between double quotes
+Console.WriteLine("\"Hello\" between double quotes");
+
+// print total 
+Console.WriteLine("Total: {0}", amountOfProduct * price);
+
+// concat strings
+string myStr = "This is my string";
+Console.WriteLine(myStr + ".");
